d07c3bbd59a630f24dc7e59a88041c5e4f3eb660
tak n <tkn.infomail@gmail.com>
2022-12-02 21:08:09 +0900

    change path

--- components/header.tsx
+++ components/header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
   const pathname = usePathname()
 
   const navItems: NavItem[] = [
-    { label: 'Home', path: '/' , icon: <FaHome color={'#999'} />, color1: '#ff9966', color2: '#ff5e62' },
+    { label: 'Home', path: '/blog' , icon: <FaHome color={'#999'} />, color1: '#ff9966', color2: '#ff5e62' },
     { label: 'About', path: '/about', icon: <FaUser color={'#999'} />, color1: '#56ccf2', color2: '#2f80ed' },
     // { label: 'Blog', path: '/blog', icon: <FaUser color={'#999'} />, color1: '#56ccf2', color2: '#2f80ed' },
     { label: 'NUOVOTAKA', path: 'https://nuovotaka.com', icon: <FaLink color={'#999'} />, color1: '#d47aff', color2: '#f6a3f0' },
